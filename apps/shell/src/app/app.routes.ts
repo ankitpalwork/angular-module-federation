4ce4243614dfa3857b5cb56669b101b5a5f656e2
lenovo <lenovo@DESKTOP-0TLLIGA>
2023-10-06 20:15:41 +0530

    angular app with micro ui

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -1,3 +1,6 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  
+];
